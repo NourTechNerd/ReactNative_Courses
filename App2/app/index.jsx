55a58b4233b168f4661841464723e0afd15d5ce6
ITSAIDI <noureddinesaidi111@gmail.com>
2025-02-01 22:31:10 +0100

    c

--- App2/app/index.jsx
+++ App2/app/index.jsx
@@ -1,22 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Text, View } from 'react-native';
 import {Link} from 'expo-router'
 
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Index</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text>Hello World</Text>
       <Link href="/profile" style={{color: 'blue'}}>Go to Profile</Link>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
