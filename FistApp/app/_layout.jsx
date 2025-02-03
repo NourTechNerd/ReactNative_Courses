5f42ba5ce3731240deb13f34376fcc6670eb520f
ITSAIDI <noureddinesaidi111@gmail.com>
2025-02-03 16:26:34 +0100

    c

--- FistApp/app/_layout.jsx
+++ FistApp/app/_layout.jsx
@@ -5,8 +5,8 @@ import {Slot} from 'expo-router'
 
 export default function App() {
   return (
-    <View className="flex flex-col">
-      <Text className= "font-bold">Header</Text>
+    <View className="flex flex-col items-center justify-center h-screen bg-primary">
+      <Text className= "text-2xl font-bold text-secondary">Header</Text>
       <Slot />
       <Text>Footer</Text>
       <StatusBar style="auto" />
